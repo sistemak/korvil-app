820db142c1375d414b9984003ce25ccb43c37bb9
sistemak <sistemak.srv@gmail.com>
2026-08-22 07:19:44 -0300

    Add files via upload

---
+++ kbd/korvil/lab/k-onversa/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('k-onversa').then(cache => {
+    return cache.addAll(['index.html']);
+  }));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
