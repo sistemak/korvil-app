9bcd1a3ed1a112f9f9df0da53b35dc8b8a14c222
sistemak <sistemak.srv@gmail.com>
2026-09-12 11:17:54 -0300

    forja instantânea: korvil/evolucao/evo_1724_plus.ts [K-AI FINAL]

---
+++ korvil/evolucao/evo_1724_plus.ts
@@ -0,0 +1,8 @@
+// EVOLUCAO +500 // #1724
+export const EVO_1724 = {
+  level: 1724,
+  power: "++500",
+  instant: true,
+  code: () => "EVO_1724_FORJADO_" + Math.random().toString(36).slice(2,8),
+  forged: "2026-09-12T14:17:32.227Z"
+};
