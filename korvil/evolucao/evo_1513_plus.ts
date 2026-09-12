060d5b6a90bc2d86ea5f5a1b6d2913645192c5ca
sistemak <sistemak.srv@gmail.com>
2026-09-12 11:17:33 -0300

    forja instantânea: korvil/evolucao/evo_1513_plus.ts [K-AI FINAL]

---
+++ korvil/evolucao/evo_1513_plus.ts
@@ -0,0 +1,8 @@
+// EVOLUCAO +500 // #1513
+export const EVO_1513 = {
+  level: 1513,
+  power: "++500",
+  instant: true,
+  code: () => "EVO_1513_FORJADO_" + Math.random().toString(36).slice(2,8),
+  forged: "2026-09-12T14:17:32.224Z"
+};
