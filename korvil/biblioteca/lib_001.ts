b56e15b5f481d7535dbb42a5f9f2a8693f53ae4e
sistemak <sistemak.srv@gmail.com>
2026-09-12 11:05:13 -0300

    forja instantânea: korvil/biblioteca/lib_001.ts [K-AI FINAL]

---
+++ korvil/biblioteca/lib_001.ts
@@ -0,0 +1,8 @@
+// BIBLIOTECA KORVIL #001
+export const LIB_001 = {
+  name: "lib_001",
+  deps: ["core","geradores","dicionarios"],
+  exec: () => console.log("LIB_001 FORJADA"),
+  version: "1.0.0",
+  integrity: "e10cc298"
+};
