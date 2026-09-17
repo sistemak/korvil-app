f82d2e941b6ed718b28310d272a00b535dbe60cf
sistemak <sistemak.srv@gmail.com>
2026-09-17 05:31:57 -0300

    K-B.D v44 - korvil-db.js - COMMIT REAL FORÇADO

--- sok/k-bd/korvil-db.js
+++ sok/k-bd/korvil-db.js
@@ -1,4 +1,4 @@
-// K-B.D v44 - 2026-09-17T08:30:29.651Z
+// K-B.D v44 - 2026-09-17T08:31:51.893Z
 export const korvilDB = {
   mem: JSON.parse(localStorage.getItem('korvil-db')||'[]'),
   criar(d){ d.id=Date.now(); d.path='korvil/'+d.nicho.toLowerCase().replace(/[^a-z0-9]+/g,'-')+'/index.html'; this.mem.push(d); localStorage.setItem('korvil-db',JSON.stringify(this.mem)); this.autoCommit(d); return d; },
