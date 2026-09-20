c08979fd445c7408ab9846b082983657218b2dcd
sistemak <sistemak.srv@gmail.com>
2026-09-20 11:20:14 -0300

    K-Onversa master real completo - 2026-09-20T14:20:14.038Z

--- korvil/sok/k-or/k-onversa/sw.js
+++ korvil/sok/k-or/k-onversa/sw.js
@@ -1,10 +1,1 @@
-const CACHE='k-onversa-v1-mestre';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate', e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch', e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
-    const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, clone)); return res;
-  }).catch(()=>caches.match('./index.html'))));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('k'))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
